b702e24eb093a6bb83d189d6071d4597603707d2
unknown <edvinas.buinovskis@stud.vgtu.lt>
2021-05-06 21:11:12 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,10 @@ import path from 'path';
 import uploadRouter from './routers/uploadRouter.js';
 import cors from 'cors';
 
-app.use(cors());
-
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/restaurant-meals-website', {
