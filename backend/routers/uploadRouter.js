3ea72eb68593256c75881c8c2422b9fd4eaa7671
unknown <edvinas.buinovskis@stud.vgtu.lt>
2021-05-25 19:00:22 +0300

    added remote image storage

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,28 +1,48 @@
-import multer from 'multer';
 import express from 'express';
 import { isAuth } from '../utilities.js';
+import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
 
+dotenv.config();
 const uploadRouter = express.Router();
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(null, `${Date.now()}.jpg`);
-    },
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 });
 
-const upload = multer({ storage });
-
-uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
-    if (req.file.path) {
-        res.send(`/${req.file.path}`);
+uploadRouter.post('/', isAuth, async (req, res) => {
+    const fileStr = req.body.data;
+    const response = await cloudinary.v2.uploader.upload(fileStr, { upload_preset: process.env.UPLOAD_PRESET });
+    // console.log(response.public_id);
+    if (response) {
+        res.send(response.public_id);
     }
     else {
         res.status(500).send({ message: 'Image not saved' });
     }
-
 });
+
+// const storage = multer.diskStorage({
+//     destination(req, file, cb) {
+//         cb(null, 'uploads/');
+//     },
+//     filename(req, file, cb) {
+//         cb(null, `${Date.now()}.jpg`);
+//     },
+// });
+
+// const upload = multer({ storage });
+
+// uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
+//     if (req.file.path) {
+//         res.send(`/${req.file.path}`);
+//     }
+//     else {
+//         res.status(500).send({ message: 'Image not saved' });
+//     }
+
+// });
 
 export default uploadRouter;
--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ var corsOptions = {
     optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/restaurant-meals-website', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
